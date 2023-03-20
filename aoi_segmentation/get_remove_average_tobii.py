b2fea3bd46d054a17728e8c44ab11fc894f96e32
Dat Duong <datd1988@gmail.com>
2023-03-20 18:13:32 -0400

    try 3 sec

--- aoi_segmentation/get_remove_average_tobii.py
+++ aoi_segmentation/get_remove_average_tobii.py
@@ -21,14 +21,21 @@ import matplotlib.pyplot as plt
 
 main_datadir = 'C:/Users/duongdb/Documents/Face11CondTobiiEyeTrack01112023'
 
+# ---------------------------------------------------------------------------- #
+
 # imdir = 'Peter25radiusTobiiHeatmap' # "25radius-fix-mismatch-name-to-csv/"
 # outputname = 'total_average_25_radiusPeter.png' # "total_average_25radius.png"
 # where_to_save_formated_individual = '25radius-keep-ave-whtbg-peter' # 25radius-fix-mismatch-name-csv-no-ave-whtbg/
 
 
-imdir = '25radius-fix-mismatch-name-to-csv' # "25radius-fix-mismatch-name-to-csv/"
-outputname = 'total_average_25radius.png' # "total_average_25radius.png"
-where_to_save_formated_individual = '25radius-fix-mismatch-name-csv-no-ave-whtbg' # 25radius-fix-mismatch-name-csv-no-ave-whtbg/
+# imdir = '25radius-fix-mismatch-name-to-csv' # "25radius-fix-mismatch-name-to-csv/"
+# outputname = 'total_average_25radius.png' # "total_average_25radius.png"
+# where_to_save_formated_individual = '25radius-fix-mismatch-name-csv-no-ave-whtbg' # 25radius-fix-mismatch-name-csv-no-ave-whtbg/
+
+imdir = '25radius-fix-mismatch-name-to-csv-3sec' # "25radius-fix-mismatch-name-to-csv/"
+outputname = 'total_average_25radius_3sec.png' # "total_average_25radius.png"
+where_to_save_formated_individual = '25radius-fix-mismatch-name-csv-no-ave-whtbg-3sec' # 25radius-fix-mismatch-name-csv-no-ave-whtbg/
+
 
 # ---------------------------------------------------------------------------- #
 
@@ -62,7 +69,7 @@ arr= Image.fromarray (np.array(np.round(arr),dtype=np.uint8))
 
 
 # https://stackoverflow.com/questions/50898034/how-replace-transparent-with-a-color-in-pillow/50898375#50898375
-out = Image.new("RGBA", ((720,720)), "WHITE")  
+out = Image.new("RGBA", ((1920,1080)), "WHITE")  
 out.paste(arr, (0, 0), arr)                  
 out = cv2.cvtColor(np.array(out), cv2.COLOR_BGR2GRAY) 
 cv2.imwrite(os.path.join(main_datadir,outputname), 255-out)
@@ -89,7 +96,7 @@ for im in imlist:
   this_img=Image.fromarray(np.array(imarr,dtype=np.uint8),mode='RGBA')
   
   # ! convert into true black/white
-  temp = Image.new("RGBA", ((720,720)), "WHITE")  
+  temp = Image.new("RGBA", ((1920,1080)), "WHITE")  
   temp.paste(this_img, (0, 0), this_img)                 
   temp = cv2.cvtColor(np.array(temp), cv2.COLOR_BGR2GRAY) 
 
@@ -101,50 +108,45 @@ for im in imlist:
 
   
 # ---------------------------------------------------------------------------- #
+# ---------------------------------------------------------------------------- #
 
 
-
-# import cv2
-# import json
-# import numpy as np
-# import pandas as pd
-# import os
-# from pathlib import Path
-# import pickle
-# from PIL import Image, ImageDraw
-# import sys, re
-
-# import matplotlib.pyplot as plt
+import cv2
+import json
+import numpy as np
+import pandas as pd
+import os
+from pathlib import Path
+import pickle
+from PIL import Image, ImageDraw
+import sys, re
+import matplotlib.pyplot as plt
 
 
-# # ! average of a few images after remove the "common consensus"
-# # imdir = 'C:/Users/duongdb/Documents/Face11CondTobiiEyeTrack01112023/25radius-fix-mismatch-name-csv-no-ave/'
+# ! average of a few images after remove the "common consensus"
 
-# imdir = 'C:/Users/duongdb/Documents/Face11CondTobiiEyeTrack01112023/RemoveAveEyeTrack/Slide11/Group1'
-# imlist = os.listdir(imdir)
+slidename = '17_'
+foutname = 'C:/Users/duongdb/Documents/Face11CondTobiiEyeTrack01112023/Slide'+slidename+'Ave3s.jpg'
+# 25radius-fix-mismatch-name-csv-no-ave-whtbg
+# 25radius-fix-mismatch-name-csv-no-ave-whtbg-3sec
+imdir = 'C:/Users/duongdb/Documents/Face11CondTobiiEyeTrack01112023/25radius-fix-mismatch-name-csv-no-ave-whtbg-3sec'
+imlist = os.listdir(imdir)
+imlist = [i for i in imlist if re.match(r'^'+slidename, i)]
 
-# # imlist = [i for i in imlist if '_BAD.png' not in i]
-# # imlist = [i for i in imlist if '_Bad.png' not in i]
-# # imlist = [i for i in imlist if '_bad.png' not in i]
-# # imlist = [i for i in imlist if 'rBAD.png' not in i]
-# # imlist = [i for i in imlist if 'POLR1C' not in i]
-
-# # imlist = [i for i in imlist if re.match(r'^5_',i) ]
-# N = len(imlist)
-# print ('total img count', N)
-# z=np.array(Image.open(os.path.join(imdir,imlist[0])),dtype=float).shape
+N = len(imlist)
+print ('total img count', N)
+z=np.array(Image.open(os.path.join(imdir,imlist[0])),dtype=float).shape
     
-# # Create a np array of floats to store the average (assume RGB images)
-# # arr=np.zeros((h,w,4),float)
-# arr=np.zeros(z,float)
+# Create a np array of floats to store the average (assume RGB images)
+# arr=np.zeros((h,w,4),float)
+arr=np.zeros(z,float)
 
-# # Build up average pixel intensities, casting each image as an array of floats
-# for im in imlist:
-#   imarr=np.array(Image.open(os.path.join(imdir,im)),dtype=float)
-#   arr=arr+imarr/N
+# Build up average pixel intensities, casting each image as an array of floats
+for im in imlist:
+  imarr=np.array(Image.open(os.path.join(imdir,im)),dtype=float)
+  arr=arr+imarr/N
 
-# # Round values in array and cast as 8-bit integer
-# arr=np.array(np.round(arr),dtype=np.uint8)
-
-# out=Image.fromarray(np.array(arr)).convert('L')
-# out.save(os.path.join('C:/Users/duongdb/Documents/Face11CondTobiiEyeTrack01112023',"total_average_no_totave_25radius_slide_11_group1.jpg"))
+# Round values in array and cast as 8-bit integer
+arr=np.array(np.round(arr),dtype=np.uint8)
+out=Image.fromarray(np.array(arr)).convert('L')
+out.save(os.path.join(foutname))
